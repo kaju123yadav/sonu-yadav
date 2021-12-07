1c64d634d1ce6b9039b143240168c849b30eda5b
kaju123yadav <95682531+kaju123yadav@users.noreply.github.com>
2021-12-07 14:46:51 +0530

    1st

---
+++ hell0.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    
+    
+    int a1,a2,total;
+    cout <<"enter the number a1";
+    cin >>a1;
+    cout<<"enter the number a2";
+    cin >>a2;
+    total=a1+a2;
+    cout <<total;
+    return 0;
+}
